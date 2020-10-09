add58ba5c82d4efe33585dea9c076ff66ae2f606
Kristian <jettkristian@gmail.com>
2020-10-08 19:03:55 -0700

    Completed bonus missions.

--- src/TechJobsConsole/JobData.cs
+++ src/TechJobsConsole/JobData.cs
@@ -15,7 +15,7 @@ namespace TechJobsConsole
         public static List<Dictionary<string, string>> FindAll()
         {
             LoadData();
-            return AllJobs;
+            return new List<Dictionary<string, string>>(AllJobs);
         }
 
         /*
@@ -37,6 +37,7 @@ namespace TechJobsConsole
                     values.Add(aValue);
                 }
             }
+            values.Sort();
             return values;
         }
 
